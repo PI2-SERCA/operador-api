574ad87c1f3c287830b35cb2d32e01e0fdc0e460
igor-paiva <igorbatistapaiva@outlook.com>
2022-09-07 14:53:29 -0300

    Change web exposed port to 5000 and operador ui to 3001

--- src/config.py
+++ src/config.py
@@ -3,5 +3,5 @@ import os
 BROKER_URL = os.environ.get("BROKER_URL", "amqp://guest:guest@localhost:5672")
 
 ALLOWED_HOSTS = os.environ.get(
-    "ALLOWED_HOSTS", "https://socketio-playground.ibrod83.com,http://localhost:3000"
+    "ALLOWED_HOSTS", "https://socketio-playground.ibrod83.com,http://localhost:3001"
 ).split(",")
