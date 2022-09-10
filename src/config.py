3dcf42caf1ccd2366079afb098c92f679ffa1a7e
igor-paiva <igorbatistapaiva@outlook.com>
2022-09-10 16:13:15 -0300

    Remove unnecessary hostname from rabbitmq service in docker-compose

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 import os
 
-BROKER_URL = os.environ.get("BROKER_URL", "amqp://guest:guest@localhost:5672")
+BROKER_URL = os.environ.get("BROKER_URL", "amqp://guest:guest@rabbitmq:5672")
 
 ALLOWED_HOSTS = os.environ.get(
     "ALLOWED_HOSTS", "https://socketio-playground.ibrod83.com,http://localhost:3001"
