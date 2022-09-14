0ba10bf32c41076f2a0ea42dc1e0541dbfe34ccd
igor-paiva <igorbatistapaiva@outlook.com>
2022-09-14 11:07:52 -0300

    Set allow_unsafe_werkzeug to true in app run

--- server.py
+++ server.py
@@ -4,4 +4,10 @@ if __name__ == "__main__":
     app, app_wrapper = create_app()
     # CAUTION use_reloader=False is to avoid reloading the app and creating a new
     # thread for the pika connection.
-    app_wrapper.run(app=app, host="0.0.0.0", debug=True, use_reloader=False)
+    app_wrapper.run(
+        app=app,
+        host="0.0.0.0",
+        debug=True,
+        use_reloader=False,
+        allow_unsafe_werkzeug=True,
+    )
